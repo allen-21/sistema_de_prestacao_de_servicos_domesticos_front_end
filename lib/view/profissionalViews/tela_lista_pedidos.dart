52e647a5056d1fffa1e69115c9c1158aaf8ffcee
Rizik-Issufo <111699081+Rizik-Issufo@users.noreply.github.com>
2024-05-27 02:24:23 +0200

    Refactorando Codigo (PedidosProfissionalScreen)

--- lib/view/profissionalViews/tela_lista_pedidos.dart
+++ lib/view/profissionalViews/tela_lista_pedidos.dart
@@ -1,13 +1,14 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 import 'package:provider/provider.dart';
 import 'package:sistema_de_prestacao_de_servicos_domesticos/models/enum/estado_pedido.dart';
 import 'package:sistema_de_prestacao_de_servicos_domesticos/viewModel/pedidos/pedido_view_model.dart';
 
-
 class PedidosProfissionalScreen extends StatelessWidget {
   final String token;
 
-  const PedidosProfissionalScreen({Key? key, required this.token}) : super(key: key);
+  const PedidosProfissionalScreen({Key? key, required this.token})
+      : super(key: key);
 
   @override
   Widget build(BuildContext context) {
@@ -28,29 +29,89 @@ class PedidosProfissionalScreen extends StatelessWidget {
               itemBuilder: (context, index) {
                 final pedido = viewModel.pedidos[index];
 
-                return ListTile(
-                  title: Text(pedido.descricao),
-                  subtitle: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Text('Cliente: ${pedido.cliente.nome}'),
-                      Text('Telefone: ${pedido.cliente.telefone}'),
-                      Text('Status: ${pedido.status}'),
-                    ],
-                  ),
-                  trailing: DropdownButton<EstadoPedido>(
-                    value: pedido.status,
-                    onChanged: (EstadoPedido? novoEstado) {
-                      if (novoEstado != null) {
-                        viewModel.alterarEstadoPedido(pedido.id, novoEstado);
-                      }
-                    },
-                    items: EstadoPedido.values.map((EstadoPedido estado) {
-                      return DropdownMenuItem<EstadoPedido>(
-                        value: estado,
-                        child: Text(estado.toString().split('.').last),
-                      );
-                    }).toList(),
+                return Center(
+                  // Adiciona um padding de 16.0 à esquerda
+                  child: Card(
+                    elevation: 2,
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(15),
+                    ),
+                    color: pedido.status.index == 0
+                        ? Colors.red[800]
+                        : Colors.blue[800],
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: <Widget>[
+                        Padding(
+                          padding: EdgeInsets.only(
+                              left: 16.0), // Espaçamento à esquerda do texto
+                          child: ListTile(
+                            title: Text(
+                              pedido.descricao.toUpperCase(),
+                              style: TextStyle(
+                                  fontSize: 24,
+                                  fontWeight: FontWeight.bold,
+                                  color: Colors.white),
+                            ),
+                            subtitle: Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                SizedBox(height: 8.0),
+                                Text(
+                                  'Cliente: ${pedido.cliente.nome}',
+                                  style: TextStyle(
+                                      fontSize: 22,
+                                      fontWeight: FontWeight.bold,
+                                      color: Colors.white),
+                                ),
+                                SizedBox(height: 8.0),
+                                Text(
+                                  'Telefone: ${pedido.cliente.telefone}',
+                                  style: TextStyle(
+                                      fontSize: 22,
+                                      fontWeight: FontWeight.bold,
+                                      color: Colors.white),
+                                ),
+                                SizedBox(height: 8.0),
+                                Text(
+                                  'Status: ${pedido.status.index == 0 ? "Pendente" : "Feito"}',
+                                  style: TextStyle(
+                                      fontSize: 22,
+                                      fontWeight: FontWeight.bold,
+                                      color: Colors.white),
+                                ),
+                                SizedBox(height: 10.0),
+                              ],
+                            ),
+                            trailing: DropdownButton<EstadoPedido>(
+                              value: pedido.status,
+                              style: TextStyle(
+                                  backgroundColor: Colors.white,
+                                  fontSize: 20.0,
+                                  color: Colors.black),
+                              onChanged: (EstadoPedido? novoEstado) {
+                                if (novoEstado != null) {
+                                  viewModel.alterarEstadoPedido(
+                                      pedido.id, novoEstado);
+                                }
+                              },
+                              items: EstadoPedido.values
+                                  .map((EstadoPedido estado) {
+                                return DropdownMenuItem<EstadoPedido>(
+                                  value: estado,
+                                  child:
+                                      Text(estado.toString().split('.').last),
+                                );
+                              }).toList(),
+                              // style: TextStyle(
+                              //     color: Colors.black,
+                              //     fontSize: 16,
+                              //     backgroundColor: Colors.white),
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
                   ),
                 );
               },
