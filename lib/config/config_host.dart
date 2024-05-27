9fbce54ca1814705eb8346b2e090f33384dc7b93
XFAlmeida <xarinadealmeida@gmail.com>
2024-05-27 10:36:52 +0200

    Update config_host.dart

--- lib/config/config_host.dart
+++ lib/config/config_host.dart
@@ -1,4 +1,9 @@
 class ConfigHost {
+<<<<<<< HEAD
   static const String baseUrl = "http://10.0.2.2:8080";
   
+=======
+  static const String baseUrl = "http://192.168.213.91:8080";
+  // static const String baseUrl = "http://10.0.2.2:8080";
+>>>>>>> c8d34013e8153e7db005f08320df7cf7c137cf4c
 }
